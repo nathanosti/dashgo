385799932fb025d1733a04edebd3362fe652370d
Nathan Osti <nathanosti.job@gmail.com>
2022-07-06 10:20:16 -0300

    ✨ Feat: set swcMinify in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: true,
+  },
   reactStrictMode: true,
-}
+  swcMinify: true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
